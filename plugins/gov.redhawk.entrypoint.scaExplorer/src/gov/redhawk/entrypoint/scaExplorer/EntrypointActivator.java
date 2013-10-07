27de30b087f777cda2aaf0a94cceb1ac87c53a6a
Mark Leone <mal@axiosengineering.com>
2013-10-07 12:46:59 -0400

    Fix checkstyle errors. Change single domain property key to match what is used in RCP app

--- plugins/gov.redhawk.entrypoint.scaExplorer/src/gov/redhawk/entrypoint/scaExplorer/EntrypointActivator.java
+++ plugins/gov.redhawk.entrypoint.scaExplorer/src/gov/redhawk/entrypoint/scaExplorer/EntrypointActivator.java
@@ -1,13 +1,12 @@
 /**
- * This file is protected by Copyright. 
- * Please refer to the COPYRIGHT file distributed with this source distribution.
- * 
- * This file is part of REDHAWK IDE.
- * 
- * All rights reserved.  This program and the accompanying materials are made available under 
- * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html.
- *
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ * 
+ * This file is part of REDHAWK IDE.
+ * 
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
  */
 package gov.redhawk.entrypoint.scaExplorer;
 
@@ -23,13 +22,13 @@ public class EntrypointActivator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "gov.redhawk.entrypoint.scaExplorer";
 
-	public static final String PROP_SINGLE_DOMAIN = "gov.redhawk.ui.singleDomain";
+	public static final String PROP_SINGLE_DOMAIN = "gov.redhawk.sca.singleDomain";
 
 	public static final String PROP_SHARED_DOMAINS = "gov.redhawk.sca.sharedDomains";
 	
-	public static String PROP_JACORB_ORB_CLASS = "org.omg.CORBA.ORBClass";
+	public static final String PROP_JACORB_ORB_CLASS = "org.omg.CORBA.ORBClass";
 	
-	public static String PROP_JACORB_ORB_SINGLETON_CLASS = "org.omg.CORBA.ORBSingletonClass";
+	public static final String PROP_JACORB_ORB_SINGLETON_CLASS = "org.omg.CORBA.ORBSingletonClass";
 
 	// The shared instance
 	private static EntrypointActivator plugin;
