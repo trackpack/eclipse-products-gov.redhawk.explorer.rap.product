4bcab9e818d355792f5393578ac58a4bc56352da
Erik Englund <erik.englund@axiosengineering.com>
2013-10-09 12:20:02 -0400

    First step in creating a simple rap server product.

--- plugins/gov.redhawk.entrypoint.scaExplorer/root/setenv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export CATALINA_OPTS="-server -Xms40m -Xmx512m -XX:PermSize=256m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -Djava.util.logging.config.file=conf/javalogger.properties -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB
--Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton -Dorg.omg.PortableInterceptor.ORBInitializerClass.standard_init=org.jacorb.orb.standardInterceptors.IORInterceptorInitializer -Djacorb.config.dir=conf/"
